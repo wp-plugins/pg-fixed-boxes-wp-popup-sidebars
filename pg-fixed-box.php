578df62ae9940254bb42b2e5e8420d1c24738a8d
f.n174 <f.n174@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-07 08:24:22 +0000

    changing version
    git-svn-id: https://plugins.svn.wordpress.org/pg-fixed-boxes-wp-popup-sidebars/trunk@961581 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pg-fixed-box.php
+++ pg-fixed-box.php
@@ -4,7 +4,7 @@ defined( 'ABSPATH' ) OR exit;
 Plugin Name: PG Fixed Boxes Plugin (WP POPUP  Sidebars)
 Plugin URI: http://fixedboxes.esy.es/wp/
 Description: Center positioned pop up box which accepts any widget of your choice ! with ability to make unlimited number of fully customizable boxes !
-Version: 2.0
+Version: 2.1
 Author: PG Team
 Author URI: http://parsigroup.net/
 License: GPL2
